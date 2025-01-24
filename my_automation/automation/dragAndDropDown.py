21966afc5f84f075fcfd7858fabe3b298db3a438
Otabek Uzbeg <otabek.yusupov20041017@gmail.com>
2025-01-24 17:31:02 +0500

    I added new file called dragAndDropDown.py

---
+++ my_automation/automation/dragAndDropDown.py
@@ -0,0 +1,64 @@
+import time
+import unittest
+from webbrowser import Chrome
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chromium.options import ChromiumOptions
+from selenium.webdriver.common.keys import Keys
+from  selenium.webdriver.common.by import By
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        options = Options()
+        options.add_experimental_option("detach", True)
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("enable-automation")
+        options.add_argument("--start-maximized")
+        options.add_argument("--lang=en-GB")
+        # Setup WebDriver (use the browser driver of your choice, e.g., Chrome, Firefox)
+        self.driver = webdriver.Chrome(options)  # Replace with `webdriver.Firefox()` for Firefox
+        self.driver.implicitly_wait(10)  # Implicit wait for elements
+        self.driver.maximize_window()
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("enable-automation")
+        options.add_argument("--start-maximized")
+
+    def test_google_search(self):
+        driver = self.driver
+        driver.get("https://demo.automationtesting.in/Static.html")
+        angular = driver.find_element(By.XPATH, "//*[@*='angular']")
+        mongo = driver.find_element(By.ID, "mongo")
+        node = driver.find_element(By.ID, "node")
+        dropBox = driver.find_element(By.XPATH, "//div[@*='droparea']")
+
+
+
+        actions = ActionChains(driver)
+        #actions.drag_and_drop(angular, dropBox).perform()
+        for angulars in range(5):
+            angulars = actions.drag_and_drop(angular, dropBox).perform()
+            print(angulars)
+            self.assertEqual(0, 0)
+
+        for mongos in range(5):
+            mongos = actions.drag_and_drop(mongo, dropBox).perform()
+            print(mongos)
+            self.assertEqual(0, 0)
+
+        for nodes in range(5):
+            nodes = actions.drag_and_drop(node, dropBox).perform()
+            print(nodes)
+            self.assertEqual(0, 0)
+
+
+
+    def tearDown(self):
+        time.sleep(7)
+        # Close the browser
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
