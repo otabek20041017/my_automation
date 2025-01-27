9c72d51ce7ee17310a8930139e94877f4022d2d9
Otabek Uzbeg <otabek.yusupov20041017@gmail.com>
2025-01-27 16:33:26 +0500

    I modified radioButtonAutomation.java file

--- src/test/java/automation_projects/automation/radioButtonAutomation.java
+++ src/test/java/automation_projects/automation/radioButtonAutomation.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class radionButtonAutomation {
+public class radioButtonAutomation {
 
     ChromeOptions options;
     WebDriver driver;
